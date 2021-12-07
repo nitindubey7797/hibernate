c714fcfd9ae16e3b20790a2a8bba381a827a3549
Mukesh Dubey (N.T.) <nitin_dubey@hcl.com>
2021-12-07 14:55:38 +0530

    hibernate

---
+++ CompanyApp-main/src/main/java/com/companyportal/app/service/EmployeeService.java
@@ -0,0 +1,31 @@
+package com.companyportal.app.service;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+import com.companyportal.app.entity.Employee;
+
+
+
+public interface EmployeeService {
+
+
+
+public void saveOrUpdateEmployee(Employee employee);
+
+
+
+public List<Employee> getEmployeesData();
+
+
+
+public void deleteEmployee(Employee employee);
+
+public Employee getEmployeeById(int employeeId);
+
+public List<Employee> searchEmployeebyName(String employeeName);
+}
